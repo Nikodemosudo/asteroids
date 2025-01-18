21ab5fc4e3d0c8d4232bed1248701be16a54c37e
Nikodemosudo <nickkolder@gmail.com>
2025-01-18 11:01:40 +0100

    added left/right rotation to player

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def  main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    
+
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,11 +20,13 @@ def  main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill((0,0,0))
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
+        fps = clock.get_fps()
+        
 
 
 
